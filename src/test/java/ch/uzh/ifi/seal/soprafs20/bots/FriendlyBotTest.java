63235dbfe63cbfccd7ef8c8ae31cfb5c39faa328
yritz <45588872+yritz@users.noreply.github.com>
2020-04-26 19:58:35 +0200

    FS20G05-46 created bots

---
+++ src/test/java/ch/uzh/ifi/seal/soprafs20/bots/FriendlyBotTest.java
@@ -0,0 +1,19 @@
+package ch.uzh.ifi.seal.soprafs20.bots;
+
+import ch.uzh.ifi.seal.soprafs20.constant.Language;
+import ch.uzh.ifi.seal.soprafs20.entity.MysteryWord;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class FriendlyBotTest {
+    Bot friendlyBot = new FriendlyBot(Language.EN);
+    MysteryWord mysteryWord = new MysteryWord();
+
+    @Test
+    public void botTest(){
+        mysteryWord.setWord("test");
+        String result = friendlyBot.getClue(mysteryWord);
+        Assertions.assertEquals("exam", result);
+    }
+}
